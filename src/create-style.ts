00bb175502e60c426d6d2f9de12aa4a10660405b
aggre <hiroyuki.aggre@gmail.com>
2018-11-20 18:20:48 +0900

    remove unnecessary code

--- src/create-style.ts
+++ src/create-style.ts
@@ -27,9 +27,9 @@ export const createStyle = <T>({ plugins = [], build }: Options<T>) => {
 		const key: [TemplateStringsArray, StyleValues] = [strings, values]
 		return cache.has(key)
 			? (cache.get(key) as T)
-			: (async processed => {
+			: (processed => {
 					const { css } = processed
-					const styleTemplate = await build(css)
+					const styleTemplate = build(css)
 					cache.set(key, styleTemplate)
 					return styleTemplate
 			  })(await transform(join(strings, values)))
